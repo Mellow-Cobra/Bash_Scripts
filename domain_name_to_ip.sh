19930d10d286b7b2d2add91aeed3748d5c4ca79a
internet.user.personal@gmail.com <internet.user.personal@gmail.com>
2024-09-03 22:57:55 -0700

    added a regex

--- domain_name_to_ip.sh
+++ domain_name_to_ip.sh
@@ -3,7 +3,8 @@
 setterm -foreground green -background blue
 
 echo "Enter domain name: "
-read domain_name 
+read domain_name
+nslookup "$domain_name" | grep -E  '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]' > /dev/pts/0
 ping $domain_name > /dev/pts/0
 
 
